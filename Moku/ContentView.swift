82c8ae411b7b04d302b6753afbe5ce3457da7ccd
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-10-11 17:22:10 +0700

    Initial Setup

---
+++ Moku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moku
+//
+//  Created by Christianto Budisaputra on 11/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moku/MokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MokuApp.swift
+//  Moku
+//
+//  Created by Christianto Budisaputra on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
