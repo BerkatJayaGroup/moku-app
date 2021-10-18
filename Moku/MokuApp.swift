84d1d293ae1cfb5ae500d80db7e82f53e9ea5203
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-10-19 00:19:24 +0700

    Create Initial Session Management Flow

--- Moku/MokuApp.swift
+++ Moku/MokuApp.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 import Firebase
 
+struct BengkelView: View {
+    @State var bengkel: Bengkel
+
+    init(from bengkel: Bengkel) {
+        _bengkel = State(wrappedValue: bengkel)
+    }
+
+    var body: some View {
+        Text("Bengkel View")
+    }
+}
+
+struct CustomerView: View {
+    @State var customer: Customer
+
+    init(from customer: Customer) {
+        _customer = State(wrappedValue: customer)
+    }
+
+    var body: some View {
+        Text("\(customer.name) - \(customer.phoneNumber)")
+    }
+}
+
 @main
 struct MokuApp: App {
+    @ObservedObject var session = SessionService.shared
+
     init() {
         FirebaseApp.configure()
     }
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if let user = session.user {
+                switch user {
+                case let .bengkel(bengkel):
+                     BengkelView(from: bengkel)
+                case let .customer(customer):
+                     CustomerView(from: customer)
+                }
+            } else {
+                // Suruh Login...
+                Text("Anda harus login.")
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            let customer = Customer(name: "Alpha", phoneNumber: "1234")
+                            session.user = .customer(customer)
+                        }
+                    }
+            }
         }
     }
 }
