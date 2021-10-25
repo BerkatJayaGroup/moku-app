698ba1b43c9ac9c8bd56341366e38f5d6c5d5edc
Dicky-27 <71161380+Dicky-27@users.noreply.github.com>
2021-10-25 17:32:16 +0700

    Update UI Ulasan

--- Moku/Customer/Components/Ulasan.swift
+++ Moku/Customer/Components/Ulasan.swift
@@ -7,21 +7,19 @@
 
 import SwiftUI
 
-
-
 struct Ulasan: View {
-    
+    var review: Review
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Devin Winardi")
+            Text("\(review.user)")
                 .font(.caption)
                 .foregroundColor(Color.gray)
-            Text("Servisnya memuaskan banget, motor langsung kenceng")
+            Text("\(review.comment ?? "no comment")")
                 .font(.body)
                 .padding(.vertical, 5)
                 .fixedSize(horizontal: false, vertical: true)
             HStack {
-                Text("07/10/2021")
+                Text(Date.convertDateFormater(date: review.timestamp))
                     .font(.caption)
                     .foregroundColor(Color.gray)
                 Spacer()
@@ -30,7 +28,7 @@ struct Ulasan: View {
                         .offset(x: 10, y: -0.5)
                         .font(.system(size: 14))
                         .foregroundColor(Color("PrimaryColor"))
-                    Text("5")
+                    Text("\(review.rating)")
                         .font(.system(size: 17))
                         .fontWeight(.heavy)
                 }
@@ -38,11 +36,13 @@ struct Ulasan: View {
         }
         
     }
+    
+   
 }
 
 struct Ulasan_Previews: PreviewProvider {
     static var previews: some View {
-        Ulasan()
+        Ulasan(review: bengkels[0].reviews[0])
             .previewLayout(.sizeThatFits)
             .padding(10)
     }
