66a4b392d2756c67abcd49e5fbc762ce3e79c848
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-10-26 17:28:44 +0700

    Update Models

---
+++ Moku/Constants/Day.swift
@@ -0,0 +1,12 @@
+//
+//  Days.swift
+//  Moku
+//
+//  Created by Christianto Budisaputra on 26/10/21.
+//
+
+import Foundation
+
+enum Day: String, Codable {
+    case senin, selasa, rabu, kamis, jumat, sabtu, minggu
+}
--- Moku/Models/Bengkel.swift
+++ Moku/Models/Bengkel.swift
@@ -6,16 +6,36 @@
 //
 
 import Foundation
+import FirebaseFirestore
 import FirebaseFirestoreSwift
 
 struct Bengkel: Codable {
+    // MARK: - Registration Related
     @DocumentID var id: String!
+    var owner: Owner
     var name: String
     var phoneNumber: String
-    var photoReference: String?
+    var location: Location
+    var operationalHours: OperationalHours
+    var operationalDays = Set<Day>()
+    var photos          = [String]()
+    var brands          = Set<Brand>()
+    var mekaniks        = [Mekanik]()
 
-//    var operationalHours: [Date]
+    // MARK: Order Related
+    var reviews         = [Review]()
+    var orders          = [Order]()
+}
 
-    var reviews = [Review]()
-    var components = [String]()
+extension Bengkel {
+    struct OperationalHours: Codable {
+        var open: Int
+        var close: Int
+    }
+
+    struct Owner: Codable {
+        let name: String
+        let phoneNumber: String
+        let email: String
+    }
 }
