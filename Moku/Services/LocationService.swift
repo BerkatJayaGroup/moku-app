0e0feea403ec754d048921b892eb112bad4aeeab
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-10-26 15:32:56 +0700

    MOS-33 Change Location (#12)
    * Finish initial implementation of current location
    
    * MOS47 - Show user's location in location button (UI)
    
    * MOS-35 - User able to search for location and set location(Backend)
    
    * Refactor Google Services
    
    - Bersihin MokuApp.swift
    - Nambahin Main View / Entry Point buat Bengkel & Customer
    
    * Hide API Key

---
+++ Moku/Services/LocationService.swift
@@ -0,0 +1,48 @@
+//
+//  LocationService.swift
+//  Moku
+//
+//  Created by Christianto Budisaputra on 26/10/21.
+//
+
+import CoreLocation
+
+final class LocationService: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+
+    static let shared = LocationService()
+
+    @Published var userCoordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+
+    private override init() {
+        super.init()
+        configureLocationManager()
+    }
+
+    private func configureLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+
+    func requestUserAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedWhenInUse {
+            manager.requestLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let recentLocation = locations.last {
+            userCoordinate = recentLocation.coordinate
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+}
