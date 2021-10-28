49ea7d2e5d0f28a96e01c69667df7edb7457e567
devin1727 <devinwinardi1727@gmail.com>
2021-10-28 17:30:17 +0700

    Add app color (#31)
    Co-authored-by: Albertus Favian <43292254+albertusfavian@users.noreply.github.com>
    Co-authored-by: Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>

---
+++ Moku/Constants/AppColor.swift
@@ -0,0 +1,18 @@
+//
+//  AppColor.swift
+//  Moku
+//
+//  Created by Devin Winardi on 28/10/21.
+//
+
+import SwiftUI
+
+struct AppColor {
+    static let accent = Color("AccentColor")
+    static let brightOrange = Color("BrightOrange")
+    static let darkOrange = Color("DarkOrange")
+    static let primaryColor = Color("PrimaryColor")
+    static let salmonOrange = Color("SalmonOrange")
+    static let lightGray = Color("LightGray")
+    static let darkGray = Color("DarkGray")
+}
