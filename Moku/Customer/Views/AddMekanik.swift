3a59bd30636ef4dc15b36e702b6e96850c221fe3
Albertus Favian <43292254+albertusfavian@users.noreply.github.com>
2021-10-28 16:10:48 +0700

    Feature/MOS-106 Bengkel Settings (#30)
    * add pengaturan bengkel page, add mekanik (not finished)
    
    * finishing pengaturan bengkel page

---
+++ Moku/Customer/Views/AddMekanik.swift
@@ -0,0 +1,58 @@
+//
+//  addMekanik.swift
+//  Moku
+//
+//  Created by Mac-albert on 26/10/21.
+//
+
+import SwiftUI
+
+struct AddMekanik: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @Binding var showSheetView: Bool
+    @State var mechanicName: String?
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image(systemName: "number")
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .cornerRadius(16)
+                Text("Tambah Foto")
+                    .font(Font.system(size: 16, weight: .regular))
+                    .foregroundColor(Color("PrimaryColor"))
+                VStack(alignment: .leading) {
+                    Text("NAMA MEKANIK")
+                        .font(Font.system(size: 11, weight: .regular))
+                    TextField("Tulis Nama Mekanik", text: $mechanicName)
+                        .frame(width: .infinity, height: 40)
+                        .background(Color(hex: "F3F3F3"))
+                        .cornerRadius(8)
+                }
+                Spacer()
+            }
+            .navigationBarTitle("Tambah Mekanik", displayMode: .inline)
+            .navigationBarItems(leading: Button(action: {
+                self.showSheetView = false
+            }) {
+                HStack {
+                    Image(systemName: "chevron.left")
+                    Text("Kembali")
+                }
+            }, trailing: Button(action: {
+                print("Dismissing sheet view...")
+
+            }) {
+                    Text("Tambah")
+            })
+            .padding()
+            .edgesIgnoringSafeArea(.leading)
+        }
+    }
+
+}
+
+struct AddMekanik_Previews: PreviewProvider {
+    static var previews: some View {
+        AddMekanik(showSheetView: .constant(true), mechanicName: "Heru")
+    }
+}
---
+++ Moku/Customer/Views/PengaturanBengkel.swift
@@ -0,0 +1,139 @@
+//
+//  PengaturanBengkel.swift
+//  Moku
+//
+//  Created by Mac-albert on 26/10/21.
+//
+
+import SwiftUI
+
+struct MotorBrand: Identifiable, Hashable {
+    var id: String {name}
+    var name: String
+}
+
+struct Motorcc: Identifiable, Hashable {
+    var id: String {ccMotor}
+    var ccMotor: String
+}
+struct SelectedBrand {
+    var brand: Set<MotorBrand>
+    var cc: Set<Motorcc>
+}
+
+let allBrands: [MotorBrand] = [MotorBrand(name: "Honda"), MotorBrand(name: "Yamaha"), MotorBrand(name: "Suzuki")]
+let allCC: [Motorcc] = [Motorcc(ccMotor: "110"), Motorcc(ccMotor: "125")]
+
+struct PengaturanBengkel: View {
+    @State private var date = Date()
+    @State private var brandMotor: String = ""
+    @State private var ccMotor: String = ""
+    @State private var isBrandSelected: Bool = false
+    @State private var isCCSelected: Bool = false
+    @State private var isAddMekanik: Bool = false
+    var dayInAWeek: [String] = ["Sen", "Sel", "Rab", "Kam", "Jum", "Sab", "Min"]
+    @State var daySelected: [Bool] = [true, true, true, true, true, true, true]
+    @State var task = SelectedBrand(brand: [], cc: [])
+    var body: some View {
+        GeometryReader { proxy in
+            NavigationView {
+                VStack(spacing: 24) {
+                    VStack(spacing: 8) {
+                        Text("BRAND MOTOR YANG BISA DIPERBAIKI")
+                            .font(Font.system(size: 11, weight: .regular))
+                            .frame(width: proxy.size.width, alignment: .leading)
+                        MultiSelector(options: allBrands,
+                                      optionToString: { $0.name },
+                                      selected: $task.brand
+                        )
+                            .frame(width: .infinity, height: 40)
+                            .background(Color(hex: "F3F3F3"))
+                            .cornerRadius(8)
+
+                    }
+                    VStack(spacing: 8) {
+                        Text("CC MOTOR YANG BISA DIPERBAIKI")
+                            .font(Font.system(size: 11, weight: .regular))
+                            .frame(width: proxy.size.width, alignment: .leading)
+                        MultiSelector(options: allCC,
+                                      optionToString: { $0.ccMotor },
+                                      selected: $task.cc
+                        )
+                            .frame(width: .infinity, height: 40)
+                            .background(Color(hex: "F3F3F3"))
+                            .cornerRadius(8)
+                    }
+                    VStack(spacing: 8) {
+                        Text("HARI OPERASIONAL")
+                            .font(Font.system(size: 11, weight: .regular))
+                            .frame(width: proxy.size.width, alignment: .leading)
+                        HStack {
+                            ForEach(0..<dayInAWeek.count) { indexDay in
+                                Text("\(dayInAWeek[indexDay])")
+                                    .padding(.all, 7)
+                                    .background(self.daySelected[indexDay] == true ? Color(hex: "EB6424") : Color(hex: "D2CFCF"))
+                                    .cornerRadius(8)
+                                    .foregroundColor(self.daySelected[indexDay] == true ? Color.white : Color.gray)
+                                    .onTapGesture {
+                                        if self.daySelected[indexDay] == true {
+                                            daySelected[indexDay] = false
+                                        } else {
+                                            daySelected[indexDay] = true
+                                        }
+                                    }
+                            }
+                        }
+                    }
+                    .frame(width: .infinity)
+                    VStack(spacing: 8) {
+                        Text("JAM OPERASIONAL")
+                            .font(Font.system(size: 11, weight: .regular))
+                            .frame(width: proxy.size.width, alignment: .leading)
+                        DatePicker(
+                            "Start Day",
+                            selection: $date,
+                            displayedComponents: .hourAndMinute
+                        )
+                        DatePicker(
+                            "End Day",
+                            selection: $date,
+                            displayedComponents: .hourAndMinute
+                        )
+                    }
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("MEKANIK")
+                            .font(Font.system(size: 11, weight: .regular))
+                            .frame(width: proxy.size.width, alignment: .leading)
+                        Button(action: {
+                            self.isAddMekanik.toggle()
+                        }) {
+                            Text("+Tambah Mekanik")
+                                .font(Font.system(size: 13, weight: .regular))
+                                .foregroundColor(Color("PrimaryColor"))
+                                .frame(width: proxy.size.width, alignment: .leading)
+                        }
+                        .sheet(isPresented: $isAddMekanik) {
+                            AddMekanik(showSheetView: self.$isAddMekanik)
+                            }
+                    }
+                    Spacer()
+                    NavigationLink(destination: AddMekanik( showSheetView: $isAddMekanik)) {
+                        Text("Lanjutkan")
+                            .padding(.vertical, 8)
+                            .frame(width: proxy.size.width * 0.8, alignment: .center)
+                            .background(Color("PrimaryColor"))
+                            .foregroundColor(Color.white)
+                            .cornerRadius(8)
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct PengaturanBengkel_Previews: PreviewProvider {
+    static var previews: some View {
+        PengaturanBengkel()
+    }
+}
