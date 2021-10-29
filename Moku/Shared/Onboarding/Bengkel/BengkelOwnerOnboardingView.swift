390476501287d87bb2e06678db8e16d312d25296
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-10-29 13:51:39 +0700

    Initial prototype

--- Moku/Shared/Onboarding/Bengkel/BengkelOwnerOnboardingView.swift
+++ Moku/Shared/Onboarding/Bengkel/BengkelOwnerOnboardingView.swift
@@ -6,17 +6,22 @@
 //
 
 import SwiftUI
-import Contacts
 
 struct BengkelOwnerOnboardingView: View {
     @State var ownerName: String = ""
     @State var bengkelName: String = ""
-    @State var bengkelAddress: String = ""
+    @State var bengkelLocation: Location?
     @State var bengkelPhoneNumber: String = ""
     @State var isNavigateActive = false
 
+    @ObservedObject var locationService = LocationService.shared
+    @State var isSelectingLocation = true
+    @State var isMapOpen = false
+    @State var gatauApaan = ""
+
     var body: some View {
         VStack {
+            NavigationLink(destination: googleMap(), isActive: $isMapOpen) { EmptyView() }
             Form {
                 Section(header: Text("NAMA PEMILIK")) {
                     TextField("Tulis namamu disini", text: $ownerName)
@@ -25,8 +30,18 @@ struct BengkelOwnerOnboardingView: View {
                     TextField("Tulis nama bengkelmu disini", text: $bengkelName)
                 }
                 Section(header: Text("ALAMAT")) {
-                    TextField("Cari alamat bengkelmu disini", text:
-                                $bengkelAddress)
+                    Button {
+                        isSelectingLocation.toggle()
+                    } label: {
+                        if let address = bengkelLocation?.address {
+                            Text(address).foregroundColor(.primary)
+                        } else {
+                            HStack {
+                                Image(systemName: "mappin.circle")
+                                Text("Cari alamat bengkelmu disini")
+                            }.foregroundColor(.tertiaryLabel)
+                        }
+                    }
                 }
                 Section(header: Text("NOMOR TELEPON BENGKEL")) {
                     TextField("08xx-xxxx-xxxx", text: $bengkelPhoneNumber).keyboardType(.numberPad)
@@ -46,7 +61,57 @@ struct BengkelOwnerOnboardingView: View {
             }
 
         }
+        .sheet(isPresented: $isSelectingLocation) {
+            selectLocation()
+        }
         .navigationBarTitle("Profil Bengkel", displayMode: .inline)
+    }
+
+    private func selectLocation() -> some View {
+        VStack {
+            HStack {
+                Spacer()
+                RoundedRectangle(cornerRadius: 8)
+                    .frame(width: 56, height: 6)
+                    .foregroundColor(.quaternaryLabel)
+                Spacer()
+            }.padding(.bottom)
+            HStack {
+                Text("Pilih Lokasimu").font(.title3.bold())
+                Spacer()
+                Button {
+                    isSelectingLocation.toggle()
+                    isMapOpen.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: "mappin.circle.fill").imageScale(.small)
+                        Text("Pilih Lokasi").font(.subheadline.bold())
+                    }
+                    .padding(.vertical, 6)
+                    .padding(.horizontal, 10)
+                    .foregroundColor(.white)
+                    .background(AppColor.primaryColor)
+                    .cornerRadius(8)
+                }
+            }
+            SearchBarLocation(text: $gatauApaan)
+            Spacer()
+        }.padding()
+    }
+
+    private func googleMap() -> some View {
+        GoogleMapView(coordinate: $locationService.userCoordinate) { coordinate in
+            locationService.userCoordinate = coordinate
+            MapHelper.geocode(absolute: true, coordinate: coordinate) { address in
+                bengkelLocation = Location(
+                    address: address,
+                    longitude: coordinate.longitude,
+                    latitude: coordinate.latitude
+                )
+            }
+        }
+        .statusBar(hidden: true)
+        .ignoresSafeArea()
     }
 }
 
