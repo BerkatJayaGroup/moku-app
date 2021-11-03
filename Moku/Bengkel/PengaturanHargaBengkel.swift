de685ebd57b271070e833fcafc5783c50278ed71
devin1727 <devinwinardi1727@gmail.com>
2021-11-03 16:15:16 +0700

    resolve conflict

--- Moku/Bengkel/PengaturanHargaBengkel.swift
+++ Moku/Bengkel/PengaturanHargaBengkel.swift
@@ -10,12 +10,8 @@ import Firebase
 import Foundation
 
 struct PengaturanHargaBengkel: View {
-<<<<<<< feature/createBengkel
     var bengkelOwnerFormViewModel: BengkelOwnerOnboardingView.ViewModel
     var bengkelOwnerForm: BengkelOwnerOnboardingView
-=======
-    var bengkelOwnerForm: BengkelOwnerOnboardingView.ViewModel
->>>>>>> dev
     var pengaturanBengkelForm: PengaturanBengkel
     @ObservedObject var bengkelViewModel: BengkelViewModel = .shared
     @State private var min: String = ""
@@ -23,7 +19,6 @@ struct PengaturanHargaBengkel: View {
     @ObservedObject var storageService: StorageService = .shared
     
     var body: some View {
-<<<<<<< feature/createBengkel
         VStack(alignment: .center) {
             Text("Masukkan harga jasa service rutin, umumnya service rutin mencakup jasa pengecekan mesin, busa filter, pengecekan komponen ban, lampu, rantai, dan lainnya.")
                 .font(.system(size: 13))
@@ -58,48 +53,6 @@ struct PengaturanHargaBengkel: View {
                 .foregroundColor(.white)
                 .clipShape(RoundedRectangle(cornerRadius: 5.0))
                 .padding(.horizontal)
-=======
-        NavigationView {
-            VStack(alignment: .center) {
-                Text("Masukkan harga jasa service rutin, umumnya service rutin mencakup jasa pengecekan mesin, busa filter, pengecekan komponen ban, lampu, rantai, dan lainnya.")
-                    .font(.system(size: 13))
-                    .padding(.bottom)
-                    .foregroundColor(.gray)
-                    .padding(.horizontal)
-                HStack {
-                    TextField("Rp minimum", text: $min)
-                        .font(.system(size: 15))
-                        .padding(10)
-                        .background(Color(.systemGray5))
-                        .cornerRadius(9)
-                        .keyboardType(.numberPad)
-                    Text("-")
-                    TextField("Rp maksimum", text: $max)
-                        .font(.system(size: 15))
-                        .padding(10)
-                        .background(Color(.systemGray5))
-                        .cornerRadius(9)
-                        .keyboardType(.numberPad)
-                }.padding()
-                Spacer()
-                Text("Harga yang Anda masukkan dapat diubah sesuai dengan kerusakan komponen dan kesepakatan dengan pelanggan")
-                    .font(.system(size: 13))
-                    .foregroundColor(.gray)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                NavigationLink(destination: BengkelView()) {
-                    Button("Lanjutkan"){ print("testing") }
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color("PrimaryColor"))
-                        .foregroundColor(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 5.0))
-                        .padding(.horizontal)
-                }
-            }
-            .padding()
-            .navigationBarTitle("Pengaturan Harga", displayMode: .inline)
->>>>>>> dev
         }
         .padding()
         .navigationBarTitle("Pengaturan Harga", displayMode: .inline)
@@ -117,15 +70,9 @@ struct PengaturanHargaBengkel: View {
 //                }
 //            }
 //        }
-=======
-    func createBengkel(bengkelOwnerForm: BengkelOwnerOnboardingView.ViewModel, pengaturanBengkelForm: PengaturanBengkel){
-        // TODO: upload foto bengkel dan simpan di object bengkel
->>>>>>> dev
         
         let calendar = Calendar.current
         let openTime = calendar.component(.hour, from: pengaturanBengkelForm.openTime)
-        let closeTime = calendar.component(.hour, from: pengaturanBengkelForm.closeTime)
-<<<<<<< feature/createBengkel
         guard let location = bengkelOwnerFormViewModel.location else {return}
         var bengkelBaru = Bengkel(
             owner: Bengkel.Owner(name: bengkelOwnerFormViewModel.ownerName, phoneNumber: bengkelOwnerFormViewModel.phoneNumber, email: ""),
@@ -134,21 +81,10 @@ struct PengaturanHargaBengkel: View {
             location: location,
             operationalHours: Bengkel.OperationalHours(open: openTime, close: closeTime),
             operationalDays: pengaturanBengkelForm.daySelected,
-=======
-        guard let location = bengkelOwnerForm.location else {return}
-        var bengkelBaru = Bengkel(
-            owner: Bengkel.Owner(name: bengkelOwnerForm.ownerName, phoneNumber: bengkelOwnerForm.phoneNumber, email: ""),
-            name: bengkelOwnerForm.bengkelName,
-            phoneNumber: bengkelOwnerForm.phoneNumber,
-            location: location,
-            operationalHours: Bengkel.OperationalHours(open: openTime, close: closeTime),
-            operationalDays: [.senin, .selasa, .rabu],
->>>>>>> dev
             minPrice: min,
             maxPrice: max
         )
         
-<<<<<<< feature/createBengkel
         for brand in pengaturanBengkelForm.selectedBrand{
             bengkelBaru.brands.insert(brand)
         }
@@ -162,18 +98,10 @@ struct PengaturanHargaBengkel: View {
             }
             else{
                 mekBaru.photo = ""
-=======
-        for mech in pengaturanBengkelForm.mechanics{
-            // TODO: upload foto mekanik and assign to photo
-            let mekBaru = Mekanik(name: mech.name)
-            if let photo = mech.photo {
-                storageService.upload(image: photo, path: mekBaru.id)
->>>>>>> dev
             }
             bengkelBaru.mekaniks.append(mekBaru)
         }
         
-<<<<<<< feature/createBengkel
         for img in bengkelOwnerForm.pickerResult {
             let imgID = UUID().uuidString
             storageService.upload(image: img, path: imgID)
@@ -183,8 +111,5 @@ struct PengaturanHargaBengkel: View {
         bengkelViewModel.create(bengkelBaru)
         
         SessionService.shared.user = .bengkel(bengkelBaru)
-=======
-        bengkelViewModel.create(bengkelBaru)
->>>>>>> dev
     }
 }
