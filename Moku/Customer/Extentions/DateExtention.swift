e936db84c1b7357ad46d3419e7e0d6ae901a7d04
Dicky-27 <71161380+Dicky-27@users.noreply.github.com>
2021-11-05 13:57:24 +0700

    Wip/mos 16 finish page available time (#45)
    * Finishing UI
    
    * Update UI
    
    * Update UI
    
    * Update
    
    * Update week function
    
    * update session
    
    * Navigation Link from bengkel detail
    
    * Create Order

--- Moku/Customer/Extentions/DateExtention.swift
+++ Moku/Customer/Extentions/DateExtention.swift
@@ -24,25 +24,36 @@ extension Date {
     private static func getComponent(date: Date, format: String) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = format
-        formatter.locale = Locale.autoupdatingCurrent
+        formatter.locale = Locale(identifier: "id_ID")
         let component = formatter.string(from: date)
         return component
     }
 
-    static func getWeek(for month: Int = 1) -> [BookDate] {
+    static func getWeek(for month: Int = 1, hari: Int) -> [BookDate] {
         var dates = [BookDate]()
         let calendar = Calendar.current
-
-        for number in 1...7 {
-            guard let fullDate = calendar.date(byAdding: DateComponents(day: number), to: Date()) else { continue }
-            let day = getComponent(date: fullDate, format: "EEE")
-            let dayNumber = getComponent(date: fullDate, format: "dd")
-            let month = getComponent(date: fullDate, format: "MM")
-            let year = getComponent(date: fullDate, format: "yy")
-            let bookDate = BookDate(day: day, dayNumber: dayNumber, month: month, year: year)
-            dates.append(bookDate)
+        if hari < 7 {
+            let lastIndex = 7 + (7 - hari)
+            for number in 0...lastIndex {
+                    guard let fullDate = calendar.date(byAdding: DateComponents(day: number), to: Date()) else { continue }
+                    let day = getComponent(date: fullDate, format: "EEE")
+                    let dayNumber = getComponent(date: fullDate, format: "dd")
+                    let month = getComponent(date: fullDate, format: "MM")
+                    let year = getComponent(date: fullDate, format: "yyyy")
+                    let bookDate = BookDate(day: day, dayNumber: dayNumber, month: month, year: year)
+                    dates.append(bookDate)
+                }
+        }else {
+            for number in 0...7 {
+                    guard let fullDate = calendar.date(byAdding: DateComponents(day: number), to: Date()) else { continue }
+                    let day = getComponent(date: fullDate, format: "EEE")
+                    let dayNumber = getComponent(date: fullDate, format: "dd")
+                    let month = getComponent(date: fullDate, format: "MM")
+                    let year = getComponent(date: fullDate, format: "yyyy")
+                    let bookDate = BookDate(day: day, dayNumber: dayNumber, month: month, year: year)
+                    dates.append(bookDate)
+                }
         }
-
         return dates
 
     }
@@ -53,6 +64,16 @@ extension Date {
             dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss z"
             guard let date = dateFormatter.date(from: dateString) else { return ""}
             dateFormatter.dateFormat = "dd/MM/yyy"
+            return  dateFormatter.string(from: date)
+
+        }
+    static func convertDateFormaterWithHour(date: Date) -> String {
+            let dateString = "\(date)"
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss z"
+            dateFormatter.locale = Locale(identifier: "id_ID")
+            guard let date = dateFormatter.date(from: dateString) else { return ""}
+            dateFormatter.dateFormat = "dd/MM/yyyy/HH"
             return  dateFormatter.string(from: date)
 
         }
