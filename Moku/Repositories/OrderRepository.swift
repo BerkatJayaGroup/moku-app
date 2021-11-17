e4c5df01ca206f2f266bf15fcd9e820e74770bd1
Albertus Favian <43292254+albertusfavian@users.noreply.github.com>
2021-11-17 17:24:15 +0700

    Feature/mob 8 accept order (#67)
    * update mechanics name that responsible to handle the motorcycle
    
    * assign mechanics and add to firebase
    
    * assign mechanics and add to firebase
    
    * assign mechanics and add to firebase
    
    * fix moku app

--- Moku/Repositories/OrderRepository.swift
+++ Moku/Repositories/OrderRepository.swift
@@ -23,7 +23,7 @@ final class OrderRepository: ObservableObject {
     // MARK: Properties
     @Published var orders = [Order]()
     @Published var filteredOrders = [Order]()
-
+    
     @Published var filteredOrdersStatus = [Order]()
 
     @Published var customerOrders = [Order]()
@@ -62,7 +62,7 @@ final class OrderRepository: ObservableObject {
         }
     }
 
-    func fetch(bengkelId: String) {
+    func fetch(bengkelId: String, completionHandler: (([Order]) -> Void)? = nil) {
         store.whereField("bengkelId", isEqualTo: bengkelId).getDocuments { snapshot, error in
             if let error = error {
                 print("Error getting stories: \(error.localizedDescription)")
@@ -75,6 +75,7 @@ final class OrderRepository: ObservableObject {
 
             DispatchQueue.global(qos: .background).async {
                 self.filteredOrders = order
+                completionHandler?(order)
             }
         }
     }
@@ -113,7 +114,7 @@ final class OrderRepository: ObservableObject {
         store.document(order.id).delete()
     }
 
-    func update(order: Order, completioHandler: ((Error?) -> Void)? = nil) {
+    func updateStatus(order: Order, completioHandler: ((Error?) -> Void)? = nil) {
         do {
             try store.document(order.id).setData(from: order, merge: true) { error in
                 completioHandler?(error)
@@ -121,5 +122,13 @@ final class OrderRepository: ObservableObject {
         } catch {
             RepositoryHelper.handleParsingError(error)
         }
+    }
+    
+    func addMekanik(orderId: String, mechanicsName: String){
+        store
+            .document(orderId)
+            .updateData(
+                ["mechanicName" : mechanicsName]
+            )
     }
 }
