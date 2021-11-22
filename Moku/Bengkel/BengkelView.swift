83dd0bdcce778cbead37cca440c4eed51494d954
Naufaldi Athallah Rifqi <39259177+naufaldirfq@users.noreply.github.com>
2021-11-22 14:55:45 +0700

    Wip/MOB-5 List of workshop's appointments (#66)
    * Finish booking list for bengkel
    
    * run swiftlint --fix
    
    * remove booking detail view
    
    * integrate list booking and detail booking
    
    * fix list appointment ui

--- Moku/Bengkel/BengkelView.swift
+++ Moku/Bengkel/BengkelView.swift
@@ -12,6 +12,20 @@ import Foundation
 struct BengkelView: View {
 
     var body: some View {
-        Text("Bengkel View")
+        TabView {
+            BookingTabItemView()
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("Booking")
+                }
+            Text("Pesanan").tabItem {
+                Image(systemName: "star")
+                Text("Pesanan")
+            }
+            Text("Bengkel").tabItem {
+                Image(systemName: "star")
+                Text("Bengkel")
+            }
+        }
     }
 }
---
+++ Moku/Bengkel/View/BookingSeeAllView.swift
@@ -0,0 +1,83 @@
+//
+//  BookingSeeAllView.swift
+//  Moku
+//
+//  Created by Naufaldi Athallah Rifqi on 16/11/21.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct BookingSeeAllView: View {
+    @ObservedObject private var viewModel: BookingTabItemViewModel = .shared
+
+    init() {
+        let navBarAppearance = UINavigationBar.appearance()
+        navBarAppearance.backgroundColor = .white
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                if let order = viewModel.bengkelOrders {
+                    let newOrder = order.filter { order in
+                        return order.status == .waitingConfirmation
+                    }
+                    ForEach(0..<newOrder.count) { index in
+                        orderCards(order: newOrder[index]).padding(10)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(color: .black.opacity(0.2), radius: 3, x: 2, y: 2)
+                            .padding(.top)
+                            .padding(.horizontal)
+                    }
+                } else {
+                    ActivityIndicator(.constant(true))
+                }
+            }
+        }.navigationBarTitle("Booking Masuk", displayMode: .inline)
+    }
+
+    private func orderCards(order: Order) -> some View {
+        VStack(alignment: .leading) {
+            Text("\(order.motor.brand.rawValue) \(order.motor.model)").font(.subheadline).fontWeight(.bold)
+            HStack {
+                Image("pemilik-bengkel")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                Text(order.typeOfService.rawValue).font(.caption)
+                Spacer()
+            }
+            HStack {
+                Image("pemilik-bengkel")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                Text(order.schedule.date()).font(.caption)
+                Spacer()
+            }
+            HStack {
+                Image("pemilik-bengkel")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                Text(order.schedule.time()).font(.caption)
+                Spacer()
+                Button("Terima") {
+
+                }.padding()
+                    .frame(width: 100, height: 30)
+                    .background(AppColor.primaryColor)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                    .font(.caption)
+            }
+        }
+    }
+}
+
+struct BookingSeeAllView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookingSeeAllView()
+    }
+}
