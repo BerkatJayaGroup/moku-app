09f37362f19e3bb10901e7810c9d31594c94a62b
Albertus Favian <favianalbert@gmail.com>
2021-11-22 15:41:58 +0700

    remove unused changes

--- Moku/MokuApp.swift
+++ Moku/MokuApp.swift
@@ -12,18 +12,17 @@ import PartialSheet
 @main
 struct MokuApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
+    
     @ObservedObject var session = SessionService.shared
-
+    
     @StateObject var appState = AppState()
-
+    
     var onboardingData = OnboardingDataModel.data
     @StateObject var sheetManager = PartialSheetManager()
-
+    
     var body: some Scene {
         WindowGroup {
-//             contentView()
-            DetailBooking(order: .preview)
+            contentView()
                 .environmentObject(sheetManager)
         }
     }
@@ -37,7 +36,6 @@ struct MokuApp: App {
             }
         } else {
             if appState.hasOnboarded {
-//                AssignMechanicsView().environmentObject(sheetManager)
                 PickRoleView()
             } else {
                 OnboardingView(data: onboardingData).environmentObject(appState)
@@ -49,26 +47,24 @@ struct MokuApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         FirebaseApp.configure()
-
+        
         SessionService.shared.setup()
-
+        
         LocationService.shared.requestUserAuthorization()
-
+        
         GooglePlacesService.register()
         GoogleMapsService.register()
-
+        
         NotificationService.register(application: application)
-
-        //        BengkelRepository.shared.add(bengkel: .preview)
-
+        
         return true
     }
-
+    
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-
+        
     }
-
+    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-
+        
     }
 }
