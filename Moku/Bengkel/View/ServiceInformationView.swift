743876b9e4a03331afe733513e5560d7582192db
Naufaldi Athallah Rifqi <naufalrifqi1602@gmail.com>
2021-11-25 16:04:58 +0700

    change icon

---
+++ Moku/Bengkel/View/ServiceInformationView.swift
@@ -0,0 +1,20 @@
+//
+//  ServiceInformationView.swift
+//  Moku
+//
+//  Created by Naufaldi Athallah Rifqi on 25/11/21.
+//
+
+import SwiftUI
+
+struct ServiceInformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ServiceInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ServiceInformationView()
+    }
+}
