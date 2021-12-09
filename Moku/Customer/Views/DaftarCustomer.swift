a666f4365f737c15f923e0a0e88f900cfe8c8001
Dicky-27 <71161380+Dicky-27@users.noreply.github.com>
2021-12-09 12:37:55 +0700

    O4

--- Moku/Customer/Views/DaftarCustomer.swift
+++ Moku/Customer/Views/DaftarCustomer.swift
@@ -19,53 +19,122 @@ struct DaftarCustomer: View {
     var body: some View {
         VStack(alignment: .center) {
             ScrollView {
-            VStack(alignment: .leading) {
-                Text("NAMA")
-                    .font(.caption2)
                 VStack(alignment: .leading) {
-                    TextField("Tulis namamu disini", text: $viewModel.name, onEditingChanged: { isChanged in
-                        if !isChanged {
-                            viewModel.validateEmptyName()
+                    Text("NAMA")
+                        .font(.caption2)
+                    VStack(alignment: .leading) {
+                        TextField("Tulis namamu disini", text: $viewModel.name, onEditingChanged: { isChanged in
+                            if !isChanged {
+                                viewModel.validateEmptyName()
+                            }
+                        })
+                            .font(.subheadline)
+                            .padding(15)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(8)
+                            .padding(.bottom)
+                        if !viewModel.nameCheck {
+                            Text("Nama wajib diisi")
+                                .offset(y: -10)
+                                .font(.caption2)
+                                .foregroundColor(Color.red)
                         }
-                    })
-                        .font(.subheadline)
-                        .padding(15)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-                        .padding(.bottom)
-                    if !viewModel.nameCheck {
-                        Text("Nama Wajib Diisi")
-                            .offset(y: -10)
+                        
+                        Text("NOMOR TELEPON")
                             .font(.caption2)
-                            .foregroundColor(Color.red)
-                    }
-
-                    Text("NOMOR TELEPON")
-                        .font(.caption2)
-                    TextField("08xx-xxxx-xxxx", text: $viewModel.nomorTelepon, onEditingChanged: { isChanged in
-                        if !isChanged {
-                            viewModel.isPhoneNumberEmpty()
+                        TextField("08xx-xxxx-xxxx", text: $viewModel.nomorTelepon, onEditingChanged: { isChanged in
+                            if !isChanged {
+                                viewModel.isPhoneNumberEmpty()
+                            }
+                        })
+                            .font(.subheadline)
+                            .padding(15)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(8)
+                            .keyboardType(.numberPad)
+                            .padding(.bottom)
+                        if !viewModel.nomorCheck {
+                            Text("Nomor telepon wajib diisi")
+                                .offset(y: -10)
+                                .font(.caption2)
+                                .foregroundColor(Color.red)
                         }
-                    })
-                        .font(.subheadline)
-                        .padding(15)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-                        .keyboardType(.numberPad)
-                        .padding(.bottom)
-
-                    Text("EMAIL")
+                        Text("EMAIL")
+                            .font(.caption2)
+                        TextField("Alamat email", text: $viewModel.email, onEditingChanged: { isChanged in
+                            if !isChanged {
+                                if viewModel.textFieldValidatorEmail() {
+                                    viewModel.isEmailValid = true
+                                } else {
+                                    viewModel.isEmailValid = false
+                                    viewModel.email = ""
+                                }
+                            }
+                        })
+                            .font(.subheadline)
+                            .padding(15)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(8)
+                            .autocapitalization(.none)
+                            .autocapitalization(.none)
+                            .padding(.bottom)
+                        if !viewModel.isEmailValid {
+                            Text("Format email tidak valid, gunakan example@domain.com")
+                                .offset(y: -10)
+                                .font(.caption2)
+                                .foregroundColor(Color.red)
+                        }
+                        if !viewModel.emailCheck {
+                            Text("Email wajib diisi")
+                                .offset(y: -10)
+                                .font(.caption2)
+                                .foregroundColor(Color.red)
+                        }
+                    }
+                    Text("MODEL MOTOR")
                         .font(.caption2)
-                    TextField("Alamat email", text: $viewModel.email, onEditingChanged: { isChanged in
-                        if !isChanged {
-                            if viewModel.textFieldValidatorEmail() {
-                                viewModel.isEmailValid = true
+                    Button {
+                        viewModel.showModal.toggle()
+                    } label: {
+                        HStack {
+                            if let motor = viewModel.motor {
+                                Text(motor.model)
+                                    .foregroundColor(.black)
                             } else {
-                                viewModel.isEmailValid = false
-                                viewModel.email = ""
+                                Image(systemName: "magnifyingglass")
+                                Text("Cari Model Motormu")
                             }
                         }
-                    })
+                        .foregroundColor(.tertiaryLabel)
+                        .font(.subheadline)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .multilineTextAlignment(.leading)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color(.systemGray6)))
+                    .sheet(isPresented: $viewModel.showModal) {
+                        MotorModal(availableMotors: allMotor,
+                                   selectedMotor: $viewModel.motor,
+                                   showingSheet: $viewModel.showModal)
+                    }
+                    if !viewModel.motorCheck {
+                        Text("Motor wajib diisi")
+                            .offset(y: -10)
+                            .font(.caption2)
+                            .foregroundColor(Color.red)
+                            .padding(.top)
+                    }
+                }.padding(20)
+                Image("MotorGray")
+                    .opacity(0.3)
+                    .padding(15)
+                
+                VStack(alignment: .leading) {
+                    Text("PLAT MOTOR")
+                        .font(.caption2)
+                    TextField("Plat Motor", text: $viewModel.licensePlate)
                         .font(.subheadline)
                         .padding(15)
                         .background(Color(.systemGray6))
@@ -73,80 +142,29 @@ struct DaftarCustomer: View {
                         .autocapitalization(.none)
                         .autocapitalization(.none)
                         .padding(.bottom)
-                    if !viewModel.isEmailValid {
-                        Text("Format email tidak valid, gunakan example@domain.com")
-                            .offset(y: -10)
-                            .font(.caption2)
-                            .foregroundColor(Color.red)
-                    }
-                }
-                Text("MODEL MOTOR")
-                    .font(.caption2)
-                Button {
-                    viewModel.showModal.toggle()
-                } label: {
-                    HStack {
-                        if let motor = viewModel.motor {
-                            Text(motor.model)
-                                .foregroundColor(.black)
-                        } else {
-                            Image(systemName: "magnifyingglass")
-                            Text("Cari Model Motormu")
-                        }
-                    }
-                    .foregroundColor(.tertiaryLabel)
-                    .font(.subheadline)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .multilineTextAlignment(.leading)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 8).fill(Color(.systemGray6)))
-                .sheet(isPresented: $viewModel.showModal) {
-                    MotorModal(availableMotors: allMotor,
-                               selectedMotor: $viewModel.motor,
-                               showingSheet: $viewModel.showModal)
-                }
-            }.padding(20)
-            Image("MotorGray")
-                .opacity(0.3)
-                .padding(15)
-
-            VStack(alignment: .leading) {
-                Text("PLAT MOTOR")
-                    .font(.caption2)
-                TextField("Plat Motor", text: $viewModel.licensePlate)
-                    .font(.subheadline)
-                    .padding(15)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .autocapitalization(.none)
-                    .autocapitalization(.none)
-                    .padding(.bottom)
-
-                Text("Tahun Beli")
-                    .font(.caption2)
-                TextField("Tahun beli", text: $viewModel.year)
-                    .keyboardType(.numberPad)
-                    .font(.subheadline)
-                    .padding(15)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .autocapitalization(.none)
-                    .autocapitalization(.none)
-                    .padding(.bottom)
-            }.padding()
-
-        }
+                    
+                    Text("Tahun Beli")
+                        .font(.caption2)
+                    TextField("Tahun beli", text: $viewModel.year)
+                        .keyboardType(.numberPad)
+                        .font(.subheadline)
+                        .padding(15)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .autocapitalization(.none)
+                        .autocapitalization(.none)
+                        .padding(.bottom)
+                }.padding()
+                
+            }
             Spacer()
-
+            
             Button {
                 if viewModel.isFormInvalid {
-                    viewModel.nameCheck = false
-                    viewModel.nomorCheck = false
-                    viewModel.motorCheck = false
-                    viewModel.isEmailValid = false
+                    viewModel.validateEmptyName()
+                    viewModel.isPhoneNumberEmpty()
+                    viewModel.isMotorEmpty()
+                    viewModel.isEmailEmpty()
                 } else {
                     NotificationService.shared.getToken { token in
                         viewModel.motor?.licensePlate = viewModel.licensePlate
@@ -162,13 +180,18 @@ struct DaftarCustomer: View {
                     }
                 }
             } label: {
-            Text("Lanjutkan")
-                .frame(width: 310, height: 50)
-                .background(viewModel.name.isEmpty || viewModel.nomorTelepon.isEmpty || viewModel.email.isEmpty ? Color(.systemGray6) : Color("PrimaryColor"))
-                .foregroundColor(viewModel.name.isEmpty || viewModel.nomorTelepon.isEmpty || viewModel.email.isEmpty ? .gray : .white)
-                .cornerRadius(10)
-                .padding()
+                Text("Lanjutkan")
+                    .fontWeight(.semibold)
+                    .padding(.vertical, 16)
+                    .frame(width: UIScreen.main.bounds.width * 0.85)
+                    .background(viewModel.name.isEmpty || viewModel.nomorTelepon.isEmpty || viewModel.email.isEmpty ? Color(.systemGray6) : Color("PrimaryColor"))
+                    .foregroundColor(viewModel.name.isEmpty || viewModel.nomorTelepon.isEmpty || viewModel.email.isEmpty ? .gray : .white)
+                    .cornerRadius(8)
+                    .padding()
             }
+        }
+        .onTapGesture {
+            dismissKeyboard()
         }
         .navigationBarTitle("Profil Diri", displayMode: .inline)
     }
