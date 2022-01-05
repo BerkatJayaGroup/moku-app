95cfcff3054a4ce407469c59ddcec88ef2e133c8
Dicky-27 <buwonodirga@gmail.com>
2022-01-05 18:49:13 +0700

    O3, O1, O2,O4

--- Moku/Shared/Onboarding/OnboardingView.swift
+++ Moku/Shared/Onboarding/OnboardingView.swift
@@ -19,48 +19,57 @@ struct OnboardingView: View {
     }
 
     var body: some View {
-        TabView(selection: $curSlideIndex) {
-            ForEach(0..<data.count) { i in
-                OnboardingStepView(data: data[i]).tag(i)
-            }
-        }
-        .tabViewStyle(.page)
-        .indexViewStyle(.page(backgroundDisplayMode: .never)).foregroundColor(Color.primary)
-        .onAppear {
-            setupAppearance()
-        }
-        if curSlideIndex == data.count - 1 {
-            SignInWithAppleToFirebase({ response in
-                if response == .success {
-                   print("logged into Firebase through Apple!")
-                   appState.hasOnboarded = true
-                } else if response == .error {
-                   print("error. Maybe the user cancelled or there's no internet")
+        VStack {
+            TabView(selection: $curSlideIndex) {
+                ForEach(0..<data.count) { i in
+                    OnboardingStepView(data: data[i]).tag(i)
                 }
-            })
-                .frame(height: 50, alignment: .center)
-                .padding(.horizontal)
-            Button("Masuk ke halaman utama", action: {
-                appState.hasOnboarded = true
-            })
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(Color("SalmonOrange"))
-                .foregroundColor(Color("PrimaryColor"))
-                .clipShape(RoundedRectangle(cornerRadius: 5.0))
-                .padding(.horizontal)
-        } else {
-            Button("Selanjutnya", action: { self.curSlideIndex += 1 })
-                .font(.system(size: 17), weight: .semibold)
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(Color("PrimaryColor"))
-                .foregroundColor(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 5.0))
-                .padding(.horizontal)
-            Button("Lewati", action: { self.curSlideIndex = data.count - 1 })
-                .foregroundColor(.gray)
-                .padding()
+            }
+            .tabViewStyle(.page)
+            .indexViewStyle(.page(backgroundDisplayMode: .never)).foregroundColor(Color.primary)
+            .onAppear {
+                setupAppearance()
+            }
+            if curSlideIndex == data.count - 1 {
+                SignInWithAppleToFirebase({ response in
+                    if response == .success {
+                       print("logged into Firebase through Apple!")
+                       appState.hasOnboarded = true
+                    } else if response == .error {
+                       print("error. Maybe the user cancelled or there's no internet")
+                    }
+                })
+                    .frame(height: 50, alignment: .center)
+                    .padding(.horizontal)
+                Button("Masuk ke halaman utama", action: {
+                    appState.hasOnboarded = true
+                })
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color("SalmonOrange"))
+                    .foregroundColor(Color("PrimaryColor"))
+                    .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                    .padding(.horizontal)
+            } else {
+                Button {
+                    self.curSlideIndex += 1
+                }label: {
+                    Text("Selanjutnya")
+                        .font(.system(size: 17), weight: .semibold)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color("PrimaryColor"))
+                        .foregroundColor(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                }.padding(.horizontal)
+                Button {
+                    self.curSlideIndex = data.count - 1
+                }label: {
+                    Text("Lewati")
+                        .foregroundColor(.gray)
+                        .padding()
+                }
+            }
         }
     }
 }
